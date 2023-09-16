86fc7eba72b7b8f86916286b9b8d3d5f0a82dae6
Vladyslav <5vladik068@gmail.com>
2023-09-16 17:26:03 +0300

    Small practise

--- section6/src/10Practice.ts
+++ section6/src/10Practice.ts
@@ -40,6 +40,21 @@ type DeepReadonly<T> = keyof T extends never ? T :
  type ReadOnlyEmpl = DeepReadonly<Employee>
 
 
+ type TupleToObject<T extends readonly string[]> = {
+    [key in T[number]]: key
+  }
+
+  const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+  type result = TupleToObject<typeof tuple>
+
+  type First<T extends any[]> = T extends [] ? never : T[0]
+
+  type arr1 = ['a', 'b', 'c']
+  type arr2 = [() => 123, { a: string }]
+
+  type head1 = First<arr1> // expected to be 'a'
+  type head2 = First<arr2> // expected to be 3
 
 
 
